f7f2d1520983ca565616e45fd0cefa35f5394db0
kimjaeyou <unknown88455144@gmail.com>
2023-05-10 01:12:23 +0900

    Revert "Merge pull request #7 from marionest98/kim"
    This reverts commit 967f5de442bba09aab1949f6284b3ce025253318, reversing
    changes made to 8fd5e3392540657d7d6208b3b61c8bc99bb59316.

--- stock_m/src/main/java/stock_m/dao/ProductDao.java
+++ stock_m/src/main/java/stock_m/dao/ProductDao.java
@@ -1,12 +1,5 @@
 package stock_m.dao;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-@Mapper
 public interface ProductDao {
-	
-	@Insert("insert into product(sno,pname,price,p_val,p_count,userid) values(#{sno},#{pname},#{price},#{p_val},#{p_count},#{userid})")
-	void insertproduct(@Param("sno")int sno, @Param("pname")String pname, @Param("price")int price, @Param("p_val")int p_val, @Param("p_count")int p_count,@Param("userid")String userid );
+
 }
