8d0c483276b6350503feedecd6080dc4667608d1
lucider <lucider1012@gmail.com>
2023-05-15 05:09:34 +0900

    검색기능 구현 + 주석

--- stock_m/src/main/java/stock_m/dto/BoardDto.java
+++ stock_m/src/main/java/stock_m/dto/BoardDto.java
@@ -1,5 +1,15 @@
 package stock_m.dto;
 
-public class BoardDto {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class BoardDto {
+    private int boardno;        // 글 번호
+    private String userid;      // 사용자 ID
+    private String subject;     // 글 제목
+    private String content;     // 글 내용
+    private Date regdate;       // 등록일
+    private int readcount;      // 조회수
 }
