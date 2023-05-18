306da77682010dc028074b73698cef87ef54ed2d
kimjaeyou <unknown88455144@gmail.com>
2023-05-18 15:20:40 +0900

    kim2 05/18 push to main 15:20

--- stock_m/src/main/java/stock_m/dao/ProductDao.java
+++ stock_m/src/main/java/stock_m/dao/ProductDao.java
@@ -46,7 +46,7 @@ public interface ProductDao {
 	@Insert("INSERT INTO cart(userid,product_pno,count,price,name) values(#{userid},#{product_pno},#{count},#{price},#{name})")
 	public int addCart(Cart cart);
 	
-	@Select("select product_pno, count, product.price, name, stock.userid, s_volume, p_count, ssum, profit from cart inner join stock inner join product inner join revenue on cart.product_pno = stock.sno and cart.product_pno = product.pno and stock.userid = revenue.userid  where cart.userid = #{userid}")
+	@Select("select product_pno, count, product.price, name, stock.userid, s_volume, p_count, ssum, profit from cart inner join stock inner join product inner join revenue on product.sno = stock.sno and cart.product_pno = product.pno and stock.userid = revenue.userid  where cart.userid = #{userid}")
 	public List<Map<String,Object>> userCart(@Param("userid")String userid);
 	
 	@Select("select * from product")
