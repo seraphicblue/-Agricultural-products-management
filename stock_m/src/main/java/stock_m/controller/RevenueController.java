b1d578e962b5fdc4cfe8a4ed8339e46ef5e5b697
leeji <ggj05034@naver.com>
2023-05-23 16:57:35 +0900

    lee push main 05/23 16:57

--- stock_m/src/main/java/stock_m/controller/RevenueController.java
+++ stock_m/src/main/java/stock_m/controller/RevenueController.java
@@ -1,5 +1,285 @@
+/*
+   Date    : 2023.05.15
+   name    : RevenueController
+   type    : Controller
+   ver     : 1.1
+   connect  : confirm.jsp,rbuy.jsp,total.jsp
+   content : 판매,구매내역 장부관리 컨트롤러
+   writer  : 이이지
+   api     : x
+  */
 package stock_m.controller;
 
-public class RevenueController {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+//import java.util.function.Predicate;
 
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.server.MimeMappings.Mapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import jakarta.websocket.server.PathParam;
+
+import stock_m.dto.BuyDto;
+import stock_m.dto.RevenueDto;
+import stock_m.dto.SellDto;
+import stock_m.dto.StockDto;
+import stock_m.service.RevenueService;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@Controller
+public class RevenueController {
+	@Autowired
+	RevenueService service;
+ 
+	  @GetMapping("/company/sellinsert") 
+	  public String sellwrite(@Param("userid") String userid ,@Param("pno")String pno ,@Param("price")int price, @Param("scount") int scount) {
+	  		userid ="1" ; 
+		  service.sellinsert(userid,pno,price,scount); 
+	  return "redirect:/company/confirm"; }
+	  
+	  @GetMapping("/company/buyinsert") 
+	  public String buywrite( @Param("userid") String userid ,@Param("pno")String pno ,@Param("price")int price, @Param("bcount") int bcount) {
+	  		userid ="1" ; 
+		  service.buyinsert(userid,pno,price,bcount); 
+	  return "redirect:/company/confirm"; }
+	
+	//@ModelAttribute("user") MemDto dto) {//1.세션서장값 dto 에넘겨줌 2. 파라미터값 저장(id는 그대로) sesssion에 저장되 내용 바뀜=>db도업데이트 해줘야함
+	//dto에 아이디값은 포함되지않았음
+
+	  @RequestMapping("company/confirm")
+		public String list(@RequestParam(name="p", defaultValue = "1") int page, Model m ) {
+		  
+			//글이 있는지 체크
+			int count = service.count();//글갯수
+			
+			if(count > 0) {
+				
+			int perPage = 10; // 한 페이지에 보일 글의 갯수
+			int startRow = (page - 1) * perPage;//0부터시작하기 때문에 1뺌
+			String userid="1";
+			  List<SellDto> rlist = service.selectOne(userid); 
+			  m.addAttribute("rlist",rlist);
+			  
+			  List<Map<String, Object>> totalList = service.totalList(userid); 
+			m.addAttribute("totalList", totalList);
+		
+			int pageNum = 5;//보여질 페이지 번호수
+			int totalPages = count / perPage + (count % perPage > 0 ? 1 : 0); //전체 페이지 수
+			
+			int begin = (page - 1) / pageNum * pageNum + 1;
+			int end = begin + pageNum -1;
+			if(end > totalPages) {
+				end = totalPages;
+			}
+			 m.addAttribute("begin", begin);
+			 m.addAttribute("end", end);
+			 m.addAttribute("pageNum", pageNum);
+			 m.addAttribute("totalPages", totalPages);
+			
+			}
+		
+			m.addAttribute("count", count);
+			
+			return "company/confirm";
+		}
+	  	
+		  @GetMapping("/company/rdelete")
+		  @ResponseBody
+		   public void delete(@RequestParam("bno") int bno) {
+			  System.out.println(bno);
+			  service.deleteRef(bno);	
+			  
+			  }
+		  
+		  @GetMapping("/company/rcheckdelete")
+		  public String rcheckdelete(@RequestParam("selectedItems[]") int[] selectedItems) {
+		    for (int selectedItem : selectedItems) {
+		      int sno = selectedItem;
+		      System.out.println(sno);
+		      service.deleteRef(sno);
+		    }
+		    return "redirect:/company/confirm";
+		  }
+		  
+
+		  
+			
+			
+			//@ModelAttribute("user") MemDto dto) {//1.세션서장값 dto 에넘겨줌 2. 파라미터값 저장(id는 그대로) sesssion에 저장되 내용 바뀜=>db도업데이트 해줘야함
+			//dto에 아이디값은 포함되지않았음
+		  @RequestMapping("/company/rsell")
+		  @ResponseBody
+			public String rselllist( ) {
+		
+				String userid="1";
+				 
+				  List<Map<String, Object>> sellList = service.sellList(userid); 
+				  Gson gson=new Gson();
+				  String rlist =gson.toJson(sellList);				
+				  System.out.println("rlist:::"+rlist);
+				return rlist;
+			}
+		  	
+			  @GetMapping("/company/revselldel")
+			   public String revselldel(@RequestParam int sno) {
+				  System.out.println(sno);
+				  service.deletesell(sno);	
+				  System.out.println("this");
+				  return "/company/confirm";
+				  }
+			  
+			  @GetMapping("/company/revsellcheckdel")
+			  public String revsellcheckdel(@RequestParam("selectedItems[]") int[] selectedItems) {
+			    for (int selectedItem : selectedItems) {
+			      int sno = selectedItem;
+			      System.out.println(sno);
+			      service.deletesell(sno);
+			    }
+			    return "redirect:/company/rsell";
+			  }
+			  @RequestMapping("/company/rbuy")
+			  @ResponseBody
+				public String rbuylist(Model m ) {
+			
+					String userid="1";
+					 
+					  List<Map<String, Object>> rbuyList = service.rbuyList(userid); 
+					  Gson gson=new Gson();
+					  String rlist =gson.toJson(rbuyList);				
+					  System.out.println(rlist);
+					return rlist;
+				}
+			  	
+				  @GetMapping("/company/revbuydel")
+				   public String revbuydel(@RequestParam int bno) {
+					  System.out.println(bno);
+					  service.deleteRef(bno);	
+					  System.out.println("this");
+					  return "/company/rbuy";
+					  }
+				  
+				  @GetMapping("/company/revbuycheckdel")
+				  public String revbuycheckdel(@RequestParam("selectedItems[]") int[] selectedItems) {
+				    for (int selectedItem : selectedItems) {
+				      int bno = selectedItem;
+				      System.out.println(bno);
+				      service.deleteRef(bno);
+				    }
+				    return "redirect:/company/rbuy";
+				  }
+				  
+
+					@GetMapping("/company/revbuyupdate")
+					public String updateForm(List<BuyDto> dto ,Model m) {
+				             service.rbuyupdate(dto);
+						     m.addAttribute("dto",dto);
+						return "redirect:/company/rbuy";
+					}
+		
+					
+					  @RequestMapping("/company/total")
+					  @ResponseBody
+						public String totallist(Model m ) {
+						 System.out.println("totallist실행");
+						 
+						  String userid="1";
+						 
+						List<Map<String, Object>> totalList = service.totalList(userid); 
+						  Gson gson=new Gson();
+						  String rlist =gson.toJson(totalList);				
+						  System.out.println("total"+rlist);
+						return rlist;
+					  }
+						  @GetMapping("company/rsearch")
+						  public String searchrcontent(String search,Model m) {
+							  System.out.println(search);
+	                          String userid="1";
+							 // List<StockDto> stockList = service.sList(userid); 
+							  
+							  List<StockDto> sList=service.searchrcontent(search);
+							  m.addAttribute("stockList",sList);
+							  m.addAttribute("search", search);
+							 
+						   return "/company/search";
+						 
+						}				
+							
+						  //chart
+						
+						@GetMapping("company/cr")
+                      public String chartr(String userid,Model m) {
+							  userid="1";
+							    Calendar calendar = Calendar.getInstance();
+						        calendar.set(Calendar.YEAR, 2023);
+						        calendar.set(Calendar.MONTH, Calendar.MAY);
+						        calendar.set(Calendar.DAY_OF_MONTH, 15);
+						        
+						        Date date = calendar.getTime();
+						        
+						        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+						        String sdate = dateFormat.format(date);
+						        
+						       
+							return"company/cr";
+						}
+						
+						@GetMapping("company/getsdate")
+						@ResponseBody
+						public ResponseEntity<String> getsdate(@RequestParam("start-date") String startDate,
+                                @RequestParam("end-date") String endDate)throws JsonProcessingException{
+							
+							
+							List<Map<String, Object>> filteredData = service.getFilteredData(startDate, endDate);
+							List<Map<String, Object>> buyData =service.getbuyData(startDate, endDate);
+							List<Map<String, Object>> totalData=service.gettotalData(startDate, endDate);
+							
+							System.out.println(filteredData);
+							System.out.println(buyData);
+							System.out.println(totalData);
+							
+							//json으로 직렬화
+							ObjectMapper om= new ObjectMapper();
+                            
+							//json문자열로 변환
+							String filteredDataJson = om.writeValueAsString(filteredData);
+							String buyDataJson = om.writeValueAsString(buyData);
+							String totalDataJson = om.writeValueAsString(totalData);
+
+							Map<String, String> responseData = new HashMap<>();
+							responseData.put("filteredData", filteredDataJson);
+							responseData.put("buyData", buyDataJson);
+							responseData.put("totalData", totalDataJson);
+							
+							String jsonResponse = om.writeValueAsString(responseData);
+							
+							return ResponseEntity.ok(jsonResponse);
+							
+						}
+						 
+
+						
 }
--- stock_m/src/main/java/stock_m/service/StockService.java
+++ stock_m/src/main/java/stock_m/service/StockService.java
@@ -1,3 +1,13 @@
+/*
+	   Date    : 2023.05.15
+	   name    : StockService
+	   type    : Service
+	   ver     : 1.1
+	   connect  : StockController
+	   content : 재고관리 서비스 클래스
+	   writer  : 이이지
+	   api     : x
+	  */
 package stock_m.service;
 
 import java.text.SimpleDateFormat;
@@ -13,6 +23,7 @@ import stock_m.dao.AdminstockDao;
 import stock_m.dao.RevenueDao;
 import stock_m.dao.StockDao;
 import stock_m.dto.AdminstockDto;
+import stock_m.dto.StockDto;
 
 @Service
 public class StockService {
@@ -27,29 +38,94 @@ public class StockService {
 	RevenueDao rdao;
 	
 	
-	public List<AdminstockDto>option(){
+	public List<AdminstockDto> option(){
 		Map<String, Object> m =new HashMap<String, Object>();
 		return admindao.option(m);
 		
 	}
 
-	/*
-	 * public void inserts(String scontent, int s_volume, String s_date) {
-	 * dao.inserts(scontent,s_volume, s_date); }
-	 */
+	
+	// public void inserts(String scontent, int s_volume, String s_date) {
+	 //dao.inserts(scontent,s_volume, s_date); }
+	//main코드 수정 23.05.23
 
-	public int checki(int s_price, String scontent, int s_volume, int s_val, String userid) {
-		System.out.println(userid);
-		int s = dao.checki(scontent,userid);
-		System.out.println(s);
-		if(s>0) {
-			rdao.updater(s_price,userid);
-			 return dao.updates(scontent,s_volume,s_val,userid);
-		}else {
-			Date now = new	 Date();
-			SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
-			rdao.updater(s_price,userid);
-			return dao.inserts(scontent,s_volume,format.format(now), s_val,userid);
-		}
+	
+	  public int checki(int s_price, String scontent, int s_volume, int s_val,
+	  String userid) { System.out.println(userid); int s =
+	  dao.checki(scontent,userid); System.out.println(s); if(s>0) {
+	  rdao.updater(s_price,userid); return
+	  dao.updates(scontent,s_volume,s_val,userid); }else { Date now = new Date();
+	  SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+	  rdao.updater(s_price,userid); return
+	  dao.inserts(scontent,s_volume,format.format(now), s_val,userid); } }
+	 
+		
+			public List<StockDto> sList(String userid){
+				return dao.sList(userid);
+			}
+			
+			
+			/* public int supdate(List<StockDto> dto) {
+				return dao.supdate(dto);
+			}*/
+			
+			public int sdelete(int sno) {
+				return dao.sdelete(sno);
+				
+			}
+			public List<StockDto> searchscontent(String search){
+				return dao.searchscontent(search);
+			}
+			public int sinsert(String scontent, int s_val, String userid, int s_volume) {
+				Map<String, Object> m = new HashMap<>();
+				
+				m.put("scontent", scontent);
+				m.put("s_val", s_val);
+				m.put("userid",userid);
+				m.put("s_volume", s_volume);
+				return dao.sinsert(m);
+			}
+
+
+			public int count() {
+				// TODO Auto-generated method stub
+				return dao.count();
+			}
+
+
+			/*public int stockupdate(List<StockDto> dto) {
+			return dao.stockupdate(dto);			
+			}*///main코드 수정 23.05.23
+
+
+			/*public int stockupdate(int sno, int s_volume) {
+	             Map<String, Object> m = new HashMap<>();
+				
+				m.put("sno", sno);
+				m.put("s_volume", s_volume);
+				return dao.stockupdate(m);
+				
+			}*///main코드 수정 23.05.23
+
+
+			 
+			
+
+
+			public int supdate(int sno, int s_volume) {
+				System.out.println("this is service : "+sno+" /// "+s_volume);
+				return dao.supdate(sno,s_volume);
+				
+			}
+
+
+
+
+		
+		
+
+		
+
 	}
-}
+
+
