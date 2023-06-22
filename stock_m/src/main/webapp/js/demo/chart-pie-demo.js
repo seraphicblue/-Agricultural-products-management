8053e7db3cd6e37ccef75743f3c4d9593e468cb8
kd <rladlsghk98@gmail.com>
2023-06-22 18:05:57 +0900

    kd 06/22 18:05

--- stock_m/src/main/webapp/js/demo/chart-pie-demo.js
+++ stock_m/src/main/webapp/js/demo/chart-pie-demo.js
@@ -145,8 +145,8 @@ function appendToCanvas(ctx, scontents, backgroundColors) {
 function generateColors(length) {
   
   
- var colors = [
-    '#4e73df','#224abe','#13855c','#1cc88a', '#36b9cc', '#258391','#f6c23e','#dda20a', '#e74a3b','#e74a3b'
+var colors = [
+    '#e74a3b','#ff6347' ,'#dda20a','#f6c23e','#1cc88a','#36b9cc','#224abe','#7f00ff','#993399','#ff69b4' 
   ];
   var backgroundColors = [];
   for (var i = 0; i < length; i++) {
